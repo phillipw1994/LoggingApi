f5428c4cc9bb6ffc21b1344d3d3cc44991410cde
Phillip Wells <phillipw1994@windowslive.com>
2021-06-29 10:07:47 +1200

    Added auth endpoint

--- LoggingAuthApi/Controllers/AuthorizationController.cs
+++ LoggingAuthApi/Controllers/AuthorizationController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OpenIdConnect.Server;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LoggingAuthApi.Controllers
 {
@@ -6,31 +13,31 @@ namespace LoggingAuthApi.Controllers
     [ApiController]
     public class AuthorizationController : ControllerBase
     {
-        //[HttpPost("~/connect/token")]
-        //[Produces("application/json")]
-        //public async Task<IActionResult> Exchange()
-        //{
-        //    var request = HttpContext.GetOpenIdConnectRequest();
-        //    if (request.IsClientCredentialsGrantType())
-        //    {
-        //        var identity = new ClaimsIdentity(OpenIdConnectServerDefaults.AuthenticationScheme);
-        //        identity.AddClaim(ClaimTypes.NameIdentifier, request.ClientId,
-        //            OpenIdConnectConstants.Destinations.AccessToken);
+        [HttpPost("~/connect/token")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Exchange()
+        {
+            var request = HttpContext.GetOpenIdConnectRequest();
+            if (request.IsClientCredentialsGrantType())
+            {
+                var identity = new ClaimsIdentity(OpenIdConnectServerDefaults.AuthenticationScheme);
+                identity.AddClaim(ClaimTypes.NameIdentifier, request.ClientId,
+                    OpenIdConnectConstants.Destinations.AccessToken);
 
-        //        // Create a new authentication ticket holding the user identity.
-        //        var ticket = new AuthenticationTicket(
-        //            new ClaimsPrincipal(identity),
-        //            new AuthenticationProperties(),
-        //            OpenIdConnectServerDefaults.AuthenticationScheme);
+                // Create a new authentication ticket holding the user identity.
+                var ticket = new AuthenticationTicket(
+                    new ClaimsPrincipal(identity),
+                    new AuthenticationProperties(),
+                    OpenIdConnectServerDefaults.AuthenticationScheme);
 
-        //        return SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
-        //    }
+                return SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
+            }
 
-        //    return BadRequest(new OpenIdConnectResponse
-        //    {
-        //        Error = OpenIdConnectConstants.Errors.UnsupportedGrantType,
-        //        ErrorDescription = "The specified grant type is not supported."
-        //    });
-        //}
+            return BadRequest(new OpenIdConnectResponse
+            {
+                Error = OpenIdConnectConstants.Errors.UnsupportedGrantType,
+                ErrorDescription = "The specified grant type is not supported."
+            });
+        }
     }
 }
